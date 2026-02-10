c56f77d0a76ae1eab2b85a955736a957df916e60
CristianInBits <128726794+CristianInBits@users.noreply.github.com>
2026-02-11 00:27:47 +0100

    feature: Crear el Repository

---
+++ backend/src/main/java/com/flashcards/backend/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.flashcards.backend.repository;
+
+import com.flashcards.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmailIgnoreCase(String email);
+
+    Optional<User> findByUsernameIgnoreCase(String username);
+
+    boolean existsByEmailIgnoreCase(String email);
+
+    boolean existsByUsernameIgnoreCase(String username);
+}
